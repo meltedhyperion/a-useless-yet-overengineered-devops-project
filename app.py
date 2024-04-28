aa7cd8898c0e1abd737735c37e779b82d3fda117
meltedhyperion <aryan12official34@gmail.com>
2024-04-28 16:07:21 +0530

    make it pretty, thats what srm wants

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import psutil
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    cpu_percent = psutil.cpu_percent()
-    mem_percent = psutil.virtual_memory().percent
+    cpu_metric = psutil.cpu_percent()
+    mem_metric = psutil.virtual_memory().percent
     Message = None
-    if cpu_percent > 80 or mem_percent > 80:
-        Message = "High CPU and Memory Usage Detected, Scale the system up!"
-    return f"CPU Utilization: {cpu_percent}% and Memory Utilization: {mem_percent}%"
+    if cpu_metric > 80 or mem_metric > 80:
+        Message = "High CPU and Memory Usage Detected, Scale up!"
+    return render_template(
+        "index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, Message=Message
+    )
 
 
 if __name__ == "__main__":
